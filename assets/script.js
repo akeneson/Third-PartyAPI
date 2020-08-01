5dd7f6a5df47927480ffb3ad0b34f54fd0c24d27
Amy Keneson <amy.l.keneson@gmail.com>
2020-07-31 20:12:55 -0500

    comments added throughout files to review over tutor sessions addition

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,20 @@
-// my attempt to add currentDay
 
 
 
+// remember this ready function, because it doesn't reload the page. Everything inside this function.
 $(document).ready(function () {
     var rightNow = moment().toString('MMMM Do YYYY, h:mm:ss a');
     var hourNow = moment().hours();
 
     function hour() {
+        // for each time-block, we grabbed the ID -- which is the hour -- and then we stored it into block.
+        // then compare it to hourNOW (this moment in time right now)
         $(".time-block").each(function (index) {
             var block = $(this).attr("id");
             console.log(hourNow);
             console.log(block);
             if (hourNow > block) {
                 $(this).addClass("past");
-
             } else if (hourNow == block) {
                 $(this).addClass("present");
             } else {
@@ -22,16 +23,18 @@ $(document).ready(function () {
         });
     }
 
-    // picking the class variable from this block
+    // picking the class variable from this block whenm the saveBtn is clicked
     $(".saveBtn").click(function () {
         var savedValue = $(this).siblings(".description").val();
         var savedTime = $(this).parent().attr("id");
-        // if something is there, save, if not create an empty array
+        // if something is there, save into saveTodo, if not create an empty array
+        // saveTodo was created here as well in the same line as the obj.
         var obj = JSON.parse(window.localStorage.getItem("saveTodo")) || [];
         obj.push({
             time: savedTime,
             value: savedValue
         })
+
         window.localStorage.setItem("saveTodo", JSON.stringify(obj));
         $(".time-block").each(function (index) {
             console.log(savedValue);
@@ -39,8 +42,11 @@ $(document).ready(function () {
 
         })
     })
+    // get the items from the array
     let saveItems = JSON.parse(localStorage.getItem("saveTodo"))
     console.log(saveItems);
+
+    // for each description, grab the id == which is the time it was saved, and store the item.
     $(".description").each(function(index, value) {
         let id = $(this).parent().attr("id");
         console.log(index, value, id);
@@ -50,7 +56,6 @@ $(document).ready(function () {
                 $(this).text(saveItems[i].value);
             }
         }
-    
     })
 
     hour();
